78a0ead3c38e066b55cf90dd9b0ad8044f79f0cf
vargaeli <129927752+vargaeli@users.noreply.github.com>
2023-04-06 10:50:57 +0300

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -2,15 +2,15 @@
         <div class="container">
             <div class="units-row">
                 <div class="unit-50">
-                    <p>Handcrafted by Afnizar Nur Ghifari</p>
+                  <!--  <p>Handcrafted by Afnizar Nur Ghifari</p>-->
                 </div>
                 <div class="unit-50">
-                    <ul class="social list-flat right">
+                 <!--   <ul class="social list-flat right">
                         <li><a href="mailto:afnizarhilmi@gmail.com"><i class="fa fa-send"></i></a></li>
                         <li><a href="http://twitter.com/afnizarnur"><i class="fa fa-twitter"></i></a></li>
                         <li><a href="http://dribbble.com/afnizarnur"><i class="fa fa-dribbble"></i></a></li>
                         <li><a href="http://behance.com/afnizarnur"><i class="fa fa-behance"></i></a></li>
-                    </ul>
+                    </ul>-->
                 </div>
             </div>
         </div>
